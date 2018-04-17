1ce468a0a2f5ea196020ecb53db301a7fa978209
unknown <nicbryan779@gmail.com>
2018-04-17 21:39:37 +0700

    Algolia

--- app/Product.php
+++ app/Product.php
@@ -3,11 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Product extends Model
 {
+
+    use Searchable;
+
     public $timestamps = true;
-    protected $table="products";
+    protected $table = "products";
     protected $fillable = ['type','name','brand','description','price','stock','sold','img','video'];
     protected $hidden = [];
+
+    public function toSearchableArray()
+      {
+            $user = $this->toArray();
+            unset($user['sold'], $user['img'],$user['video'],$user['id'],$user['stock'],$user['created_at'],$user['updated_at'],$user['ObjectID']);
+            return $user;
+      }
 }
--- config/app.php
+++ config/app.php
@@ -147,6 +147,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+        Laravel\Scout\ScoutServiceProvider::class,
 
         /*
          * Package Service Providers...
